1d42e1ea2a8ec2d62d771695d990bed8ab2f4e56
Harrison-Rotich <sirharrison673@gmail.com>
2025-09-19 15:15:22 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*************************Hari Tech*******************************");
+        System.out.println("#99 ACS Building Gilmore Ave. New Mla.. Quezon City * Tel. No:123-4567");
+        System.out.println("\t\tVAT REG. E032-01-1646/2024");
+        System.out.println("\t\t\tSALES INVOICE\n");
+
+        System.out.println("Sold to:Jim Kim\t\t\tDate:28/04/2020");
+        System.out.println("Address:private bag\t\t\tContact Number:0100100100\n");
+
+        System.out.println("===============================================================");
+        System.out.println("Qty\t| Item Description\t| Unit Price\t| Amount");
+        System.out.println("===============================================================");
+        System.out.println("4\t| Pencil\t\t| 20\t\t| 80.00");
+        System.out.println("10\t| Duster\t\t| 50\t\t| 500.00");
+        System.out.println("9\t| Pens\t\t\t| 30\t\t| 270.00");
+        System.out.println("10\t| crayon\t\t| 80\t\t| 800.00");
+        System.out.println("===============================================================");
+        System.out.println("\t\t\t\t|Sub Total\t| 1650.00");
+        System.out.println("\t\t\t\t=========================");
+        System.out.println("\t\t\t\t|VAT(16%)\t| 264.00");
+        System.out.println("\t\t\t\t=========================");
+        System.out.println("\t\t\t\t|Total Amount\t| 1914.00");
+        System.out.println("\t\t\t\t=========================");
+    }
+}
